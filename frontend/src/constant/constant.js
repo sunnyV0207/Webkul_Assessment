cc1d2afba9e0fb12c539ce43d1bbb49dd2020ef3
Sunny Verma <sunnyvermaverma2005@gmail.com>
2025-11-04 11:40:18 +0530

    update backend URLs in .env and constant.js for production environment

--- frontend/src/constant/constant.js
+++ frontend/src/constant/constant.js
@@ -2,4 +2,4 @@
 export const BACKEND_URL=import.meta.env.VITE_BACKEND_URL
 export const API_URL=`${BACKEND_URL}api`
 
-export const MEDIA_URL="http://127.0.0.1:8000/media/"
+export const MEDIA_URL="https://webkul-assessment.onrender.com/media/"
